989b66d6ffaf8613fc388d9b2a88081b848613ba
FrancoFantomius <56490563+FrancoFantomius@users.noreply.github.com>
2020-06-25 11:26:58 +0200

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,14 @@
+import socket
+
+max_h=10
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))#utlizzare la vera porta nella versione finale
+s.listen(15)
+
+while True:
+    clientsocket, address=s.accept()
+    print(f"{address}")
+    msg="Connesso al Server"
+    msg=f"{len(msg):<{max_h}}"+msg
+    clientsocket.send(bytes(msg, "utf-8"))
